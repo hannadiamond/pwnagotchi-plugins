e258539400d9ace8901419ea5f494f4c6e5d25cd
Hanna.Diamond <toon1334@gmail.com>
2024-10-25 18:17:48 -0700

    Update age.py

--- plugins/age.py
+++ plugins/age.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+from datetime import datetime
 
 import pwnagotchi
 import pwnagotchi.plugins as plugins
@@ -17,8 +18,8 @@ class Age(plugins.Plugin):
     __description__ = 'A plugin that will add age and strength stats based on epochs and trained epochs'
 
     def __init__(self):
-        self.epochs = 0
         self.train_epochs = 0
+        self.device_start_time = datetime.now()
 
     def on_loaded(self):
         data_path = '/root/brain.json'
@@ -41,7 +42,7 @@ class Age(plugins.Plugin):
             ui.remove_element('Strength')
 
     def on_ui_update(self, ui):
-        ui.set('Age', str(self.abrev_number(self.epochs)))
+        ui.set('Age', str(self.calculate_device_age())
         ui.set('Strength', str(self.abrev_number(self.train_epochs)))
 
 
@@ -49,13 +50,9 @@ class Age(plugins.Plugin):
         self.train_epochs += 1
         if self.train_epochs % 100 == 0:
             self.strength_checkpoint(agent)
-
-    def on_epoch(self, agent, epoch, epoch_data):
-        self.epochs += 1
-        if self.epochs % 100 == 0:
             self.age_checkpoint(agent)
 
-    def abrev_number(self, num):
+    def abrev_nuber(self, num):
         if num < 100000:
             return str(num)
         else:
@@ -69,7 +66,7 @@ class Age(plugins.Plugin):
     def age_checkpoint(self, agent):
         view = agent.view()
         view.set('face', faces.HAPPY)
-        view.set('status', "Wow, I've lived for " + str(self.abrev_number(self.epochs)) + " epochs!")
+        view.set('status', "Wow, I've lived for " + self.calculate_device_age())
         view.update(force=True)
 
     def strength_checkpoint(self, agent):
@@ -79,10 +76,22 @@ class Age(plugins.Plugin):
                            "I've trained for " + str(self.abrev_number(self.train_epochs)) + " epochs")
         view.update(force=True)
 
+    def calculate_device_age(self):
+        current_time = datetime.now()
+        age_delta = current_time - self.device_start_time
+
+        years = age_delta.days // 365
+        remaining_days = age_delta.days % 365
+        months = remaining_days // 30
+        days = remaining_days % 30
+
+        age_str = f'{years}y {months}m {days}d'
+        return age_str
+
     def load_data(self, data_path):
         if os.path.exists(data_path):
             with open(data_path) as f:
                 data = json.load(f)
-                self.epochs = data['epochs_lived']
+                self.device_start_time = data['age']
                 self.train_epochs = data['epochs_trained']
 
